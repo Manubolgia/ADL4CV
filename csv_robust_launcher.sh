1f02a9ab22225f26f925c6910fa796b386e206ec
Manuel Obelleiro <84449649+Manubolgia@users.noreply.github.com>
2023-07-06 22:42:34 +0200

    Update csv_robust_launcher.sh

--- csv_robust_launcher.sh
+++ csv_robust_launcher.sh
@@ -2,7 +2,7 @@
 
 # Directory settings
 base_dir="/home/pavkovic/ADL4CV/eval_robustness"
-csv_out="/home/obelleiro/ADL4CV/csv_out"
+csv_out="/home/pavkovic/ADL4CV/csv_out"
 mkdir -p $csv_out
 
 # Only dataset
@@ -11,32 +11,46 @@ dataset="omni_chair"
 # List of models
 models=("_wN0_wD0" "_wN0-001_wD0" "_wN0_wD0-01" "_wN0-001_wD0-01")
 
-# List of metrics
-metrics=("dtu_shading_eval" "mesh_eval")
+# Prepare header for CSV file
+header="run"
+for model in ${models[@]}; do
+    clean_model_name=$(echo $model | tr -d "_")
+    header="$header,fscore_$clean_model_name,loss_$clean_model_name,psnr_dtu_$clean_model_name,psnr_nerf_$clean_model_name"
+done
 
 # Create CSV file
 output_file="${csv_out}/${dataset}.csv"
-echo "run,fscore_model1,loss_model1,psnr_model1,fscore_model2,loss_model2,psnr_model2,fscore_model3,loss_model3,psnr_model3,fscore_model4,loss_model4,psnr_model4" > $output_file
+echo $header > $output_file
 
 # For each number from 0 to 9
 for number in {0..9}; do
     row="$number"
-    # For each model and metric
+    # For each model
     for model in ${models[@]}; do
         fscore="NaN"
         loss="NaN"
-        psnr="NaN"
-        for metric in ${metrics[@]}; do
-            # File path
-            eval_file="${base_dir}/${dataset}_${number}${model}_${metric}.txt"
-            # If file exists, extract the metrics
-            if [ -f "$eval_file" ]; then
-                fscore=$(grep "fscore:" $eval_file | awk '{print $2}')
-                loss=$(grep "loss:" $eval_file | awk '{print $2}')
-                psnr=$(grep "psnr score:" $eval_file | awk '{print $3}')
-            fi
-        done
-        row="$row,$fscore,$loss,$psnr"
+        psnr_dtu="NaN"
+        psnr_nerf="NaN"
+        # File path for mesh_eval
+        mesh_eval_file="${base_dir}/${dataset}_${number}${model}_mesh_eval.txt"
+        # If file exists, extract the fscore and loss
+        if [ -f "$mesh_eval_file" ]; then
+            fscore=$(grep "fscore:" $mesh_eval_file | awk '{print $2}')
+            loss=$(grep "loss:" $mesh_eval_file | awk '{print $2}')
+        fi
+        # File path for dtu_shading_eval
+        dtu_shading_eval_file="${base_dir}/${dataset}_${number}${model}_dtu_shading_eval.txt"
+        # If file exists, extract the psnr
+        if [ -f "$dtu_shading_eval_file" ]; then
+            psnr_dtu=$(grep "psnr score:" $dtu_shading_eval_file | awk '{print $3}')
+        fi
+        # File path for nerf_shading_eval
+        nerf_shading_eval_file="${base_dir}/${dataset}_${number}${model}_nerf_shading_eval.txt"
+        # If file exists, extract the psnr
+        if [ -f "$nerf_shading_eval_file" ]; then
+            psnr_nerf=$(grep "psnr score:" $nerf_shading_eval_file | awk '{print $3}')
+        fi
+        row="$row,$fscore,$loss,$psnr_dtu,$psnr_nerf"
     done
     echo $row >> $output_file
 done
